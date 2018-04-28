89c415bfb9ccce39725042a2ce9bcdce8b442ad8
Jorge Alejandro Quiroz Serna (Jako) <alejo.jko@gmail.com>
2018-04-27 20:32:29 -0500

    Initialize project

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$rootDir = $_SERVER['DOCUMENT_ROOT'];
+$ds = DIRECTORY_SEPARATOR;
+$directories = scandir($rootDir);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Xampp Dashboard</title>
+</head>
+<body>
+    <h1>Xammp Dashboard</h1>
+</body>
+</html>
