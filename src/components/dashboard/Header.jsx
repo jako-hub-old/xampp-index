eac11992f3ceb79c49a278ee67b988fe44aee61a
Jorge Alejandro Quiroz Serna (Jako) <alejo.jko@gmail.com>
2018-05-02 14:21:13 -0500

    Se finaliza la aplicación base.

---
+++ src/components/dashboard/Header.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class Header extends Component {
+    render() {
+        const title = "Xampp Dashboard";
+        return (
+            <div>Test</div>
+        );
+    }
+}
---
+++ src/components/dashboard/Layout.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+
+export default class Layout extends Component {
+    render() {
+        return (
+            <p>Hello world</p>
+        );
+    }
+}
